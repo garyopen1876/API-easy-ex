e6a34b822be07e4d033d48d4e746152ee370e8c7
unknown <garyopen1876@gmail.com>
2022-07-01 11:08:01 +0800

    better

--- index.js
+++ index.js
@@ -11,7 +11,30 @@ server.use(express.static(__dirname + "/public"));
 
 //router
 server.get("/", (req, res) => {
-  return res.status(200).json({ header: req.headers });
+  const ip = req.headers["x-forwarded-for"];
+  const user_agent = req.headers["user-agent"];
+  let browserName = "";
+  if (user_agent.match(/chrome|chromium|crios/i)) {
+    browserName = "chrome";
+  } else if (user_agent.match(/firefox|fxios/i)) {
+    browserName = "firefox";
+  } else if (user_agent.match(/safari/i)) {
+    browserName = "safari";
+  } else if (user_agent.match(/opr\//i)) {
+    browserName = "opera";
+  } else if (user_agent.match(/edg/i)) {
+    browserName = "edge";
+  } else {
+    browserName = "No browser detection";
+  }
+  console.log(browserName);
+  return res
+    .status(200)
+    .json({
+      ip: ip,
+      user_agent: user_agent,
+      message: "哈哈哈 你是使用" + browserName + "瀏覽器",
+    });
 });
 
 //port
